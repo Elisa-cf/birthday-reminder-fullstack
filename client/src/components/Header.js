51c3628beeeab947ac55eef2a57cdc44b4803107
Elisa <elisacanyelles@gmail.com>
2022-02-02 19:08:17 +0100

    changed color button hide/show

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import Button from './Button';
 const Header = ({showAdd, onAdd}) => {
   return (<header>
    <h2>Birthday reminder</h2>
-    <Button color={showAdd ? "#4ED4AC" : "#4ED4AC"} text={showAdd ? "Hide form" : "Show form"} 
+    <Button color={showAdd ? "#ff4154" : "#ff4154"} text={showAdd ? "Hide form" : "Show form"} 
    onClick={onAdd}/>
   </header>)
 };
