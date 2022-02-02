49f53d681484c7fe61d8cc1d2297b607eefa83a5
Elisa <elisacanyelles@gmail.com>
2022-02-02 19:07:39 +0100

    created navbar + styled it

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from '../data';
+import styled from 'styled-components'
+
+
+const Navbar = () => {
+ const [showLinks, setShowLinks] = useState(false);
+ const linksContainerRef = useRef(null);
+ const linksRef = useRef(null);
+ const toggleLinks = () => {
+  setShowLinks(!showLinks);
+ };
+ useEffect(() => {
+  const linksHeight = linksRef.current.getBoundingClientRect().height;
+  if (showLinks) {
+   linksContainerRef.current.style.height = `${linksHeight}px`;
+  } else {
+   linksContainerRef.current.style.height = '0px';
+  }
+ }, [showLinks]);
+ return (
+  <nav>
+   <div className='nav-center'>
+    <div className='nav-header'>
+     <StyledLogo>Full-Stack App</StyledLogo>
+     <button className='nav-toggle' onClick={toggleLinks}>
+      <FaBars />
+     </button>
+    </div>
+    <div className='links-container' ref={linksContainerRef}>
+     <ul className='links' ref={linksRef}>
+      {links.map((link) => {
+       const { id, url, text } = link;
+       return (
+        <li key={id}>
+         <a href={url}>{text}</a>
+        </li>
+       );
+      })}
+     </ul>
+    </div>
+    <ul className='social-icons'>
+     {social.map((socialIcon) => {
+      const { id, url, icon } = socialIcon;
+      return (
+       <li key={id}>
+        <a href={url} target="_blank">{icon}</a>
+       </li>
+      );
+     })}
+    </ul>
+   </div>
+  </nav>
+ );
+};
+
+const StyledLogo = styled.h4`
+color: #13795b;
+`
+
+export default Navbar;
