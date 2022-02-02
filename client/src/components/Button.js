65bf47c952d5796543b90ab5a955f9fa9bf284c9
Elisa <elisacanyelles@gmail.com>
2022-02-02 19:08:42 +0100

    added styled components in button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,14 +1,27 @@
 import React from 'react';
-
+import styled from 'styled-components'
 
 
 const Button = ({color, text, onClick}) => {
- return <button onClick={onClick}
+ return <StyledButton onClick={onClick}
   style={{ backgroundColor: color, marginBottom: "20px"}}
-  className="btn">
+  >
   {text}
- </button>
+ </StyledButton>
 };
 
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: flex-end;
+  background: #ff4154;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.2rem;
+`
 
 export default Button;
