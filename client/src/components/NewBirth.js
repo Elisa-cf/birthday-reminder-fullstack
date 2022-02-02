2cd52f3d103ab62581c460bf481165c946386ded
Elisa <elisacanyelles@gmail.com>
2022-02-02 16:18:46 +0100

    handled images with cloudinary

--- client/src/components/NewBirth.js
+++ client/src/components/NewBirth.js
@@ -6,11 +6,10 @@ import { useMutation } from "react-query";
 const NewBirth = ({ refetchBirthdays }) => {
   const [newName, setNewName] = useState("");
   const [newAge, setNewAge] = useState("");
-  const [newUrl, setNewUrl] = useState("");
+  const [newUrl, setNewUrl] = useState("https://cdn.shopify.com/s/files/1/1061/1924/products/Happy_Emoji_Icon_5c9b7b25-b215-4457-922d-fef519a08b06_large.png?v=1571606090");
   const [newReminder, setReminder] = useState(false);
 
 
-  //
   //Implemented useMutation and Axios to make a get request to the Rest API
 
   const client = axios.create({
@@ -26,12 +25,38 @@ const NewBirth = ({ refetchBirthdays }) => {
 
   const newAnniversaryHandler = async (e) => {
     e.preventDefault();
+
+    if (newUrl === "") {
+      alert("You must upload an image")
+      return
+    }
+
     await createNewBirthMutation.mutateAsync();
     setNewName("");
     setNewAge("");
     setNewUrl("");
     setReminder("")
     await refetchBirthdays();
+  }
+
+
+  //handling images with cloudinary in react (script in index.html in public folder)
+
+  const handleUpload = () => {
+    // create the widget
+    const widget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: 'dcpeaminc', 
+        uploadPreset: 'kixqt1da',
+      },
+      (error, result) => {
+        if (result.event === 'success') {
+          console.log(result);
+          setNewUrl(result.info.secure_url);
+        }
+      },
+    );
+    widget.open(); // open up the widget after creation
   }
 
 
@@ -58,7 +83,20 @@ const NewBirth = ({ refetchBirthdays }) => {
           required
         />
       </div>
-      <div className="form-control">
+       <div className="form-control">
+        {/*<label for="img">Upload an image:</label>
+        <input
+          type="file"
+          name="myfile"
+          id="img"
+          placeholder="copy an image address"
+          value={newUrl}
+          onChange={(e) => setNewUrl(e.target.value)}
+          required
+        />*/}
+        <button onClick={handleUpload}>Upload an image</button>
+      </div>
+      {/* <div className="form-control">
         <label for="img">Paste your avatar in url format:</label>
         <input
           type="text"
@@ -68,7 +106,7 @@ const NewBirth = ({ refetchBirthdays }) => {
           onChange={(e) => setNewUrl(e.target.value)}
           required
         />
-      </div>
+      </div> */}
       <div className="form-control form-control-check">
         <label>Important</label>
         <input
