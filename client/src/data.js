ce64a0eda01864073e3665c7b2a54d7e8ea534c6
Elisa <elisacanyelles@gmail.com>
2022-03-08 14:24:04 +0100

    deleted array of objects

--- client/src/data.js
+++ client/src/data.js
@@ -1,25 +1,6 @@
 import { FaLinkedin } from "react-icons/fa";
 import { AiOutlineGithub } from "react-icons/ai"
 
-export const links = [
-  // {
-  //   id: 1,
-  //   url: "/",
-  //   text: "home",
-  // },
-  { 
-  id: 2,
-  url: "https://elisa-canyelles-portfolio.netlify.app/",
-  text: "about",
-  },
-  {
-    id: 3,
-    url: "https://elisa-canyelles-portfolio.netlify.app/#work",
-    text: "projects",
-  },
- 
-]
-
 
 export const social =  [ 
   {
