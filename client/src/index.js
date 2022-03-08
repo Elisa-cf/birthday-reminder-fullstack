36404c4f1fde09aedf9df415ab7b44464a250ffa
Elisa <elisacanyelles@gmail.com>
2022-03-08 15:17:06 +0100

    deleted browserouter code

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 import {QueryClient, QueryClientProvider} from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
@@ -12,10 +11,8 @@ const queryClient = new QueryClient()
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
-    </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
