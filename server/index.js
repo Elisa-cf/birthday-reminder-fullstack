895e4d178f76a76af00c856bfdc7f82ad6c6c0eb
Elisa <elisacanyelles@gmail.com>
2022-03-09 09:42:20 +0100

    edited path resolve

--- server/index.js
+++ server/index.js
@@ -114,24 +114,23 @@ setupRoutes(app)
 // });
 
 
-// // Serve static assets if in production. 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) => { 
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   });
-// }
-
-// Serve static assets if in production.
+//Serve static assets if in production. 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => { 
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   });
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', '404.html'))
-  });
 }
+
+if (true) {
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve('index.html'))
+  })
+  app.get(/.*\/(?!api)/, (req, res) => {
+    res.sendFile(path.resolve('404.html'))
+  })
+}
+
 
 app.listen(serverPort);
 
