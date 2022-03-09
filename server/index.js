973373d1d40a8c545ae497feb4e1330d9a01ae4b
Elisa <elisacanyelles@gmail.com>
2022-03-09 09:47:00 +0100

    added require path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const serverPort = process.env.PORT || 5000;
 const app = express();
 const cors = require("cors");
+const path = require('path'); 
 
 const {setupRoutes} = require ('./routes');
 
